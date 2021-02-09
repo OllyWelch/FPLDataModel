405b7949d20b2aff2dbff6a3194b7077a42efb2a
Olly Welch <ollywelch04@gmail.com>
2021-02-09 12:09:41 +0000

    Added lambda handler to main.py

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from data_collection import data_collection
 from get_player_info import get_player_info
 from modelling import modelling
 
-# Do the data collection step
-data_collection()
+def handler(event, context):
+    # Get database Uri from Lambda event
+    db_uri = event['DB_URI']
 
-# Do player info collection
-get_player_info()
+    # Do the data collection step
+    data_collection(db_uri=db_uri)
 
-# Do modelling
-modelling()
+    # Do player info collection
+    get_player_info(db_uri=db_uri)
+
+    # Do modelling
+    modelling(db_uri=db_uri)
+
+    return {
+        'status_code': 200,
+        'body': 'Function run successfully'
+    }

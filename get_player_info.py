6568eaa46f8a32d738231b1bb7118c21187d6aa5
Olly Welch <ollywelch04@gmail.com>
2021-02-10 18:02:06 +0000

    Added player positions to player_info and changed now_cost -> current_price

--- get_player_info.py
+++ get_player_info.py
@@ -10,12 +10,20 @@ def get_player_info(db_uri=os.environ.get('DB_URI')):
     request_json = bootstrap_request.json()
 
     players = pd.read_json(json.dumps(request_json["elements"]))[['id', 'first_name', 
-    'second_name', 'team', 'now_cost']]
+    'second_name', 'team', 'now_cost', 'element_type']]
     teams = pd.read_json(json.dumps(request_json["teams"]))[['id', 'name', 'short_name']]
     teams = teams.rename(columns={'id': 'team', 'name': 'team_name', 'short_name': 'team_short_name'})
     teams = teams.set_index('team')
-    players = players.join(teams, on='team', how='inner')
+    players = players.join(teams, on='team', how='inner').drop(columns='team')
     players['now_cost'] = players['now_cost'] / 10
+    type_mapping = {
+        1: "GKP",
+        2: "DEF",
+        3: "MID",
+        4: "FWD"
+    }
+    players = players.replace({'element_type': type_mapping})
+    players = players.rename(columns={'now_cost': 'current_price', 'element_type': 'position'})
     players.set_index('id', inplace=True)
 
     db_uri += '?charset=utf8'
